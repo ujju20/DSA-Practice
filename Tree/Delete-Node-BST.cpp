beb4dadf6898d86bb53ba03953a198958fb261f4
Ujjawal Sharma <64632969+ujju20@users.noreply.github.com>
2022-02-14 17:06:32 +0530

    Create Delete-Node-BST.cpp

---
+++ Tree/Delete-Node-BST.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* helper(TreeNode* node)
+    {
+        if(node->left==NULL)
+            return node->right;
+        else if(node->right==NULL)
+            return node->left;
+        TreeNode* rightChild=node->right;
+        TreeNode* lastR=lastRight(node->left);
+        lastR->right=rightChild;
+        return node->left;
+    }
+    TreeNode* lastRight(TreeNode* node)
+    {
+        if(!node->right)
+            return node;
+        return lastRight(node->right);
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return NULL;
+        if(root->val==key)
+        {
+            return helper(root);
+        }
+        TreeNode* dummy=root;
+        while(root!=NULL)
+        {
+            if(root->val>key)
+            {
+                if(root->left!=NULL and root->left->val==key)
+                {
+                    root->left=helper(root->left);
+                    break;
+                }
+                else
+                {
+                    root=root->left;
+                }
+            }
+            else
+            {
+                if(root->right!=NULL and root->right->val==key)
+                {
+                    root->right=helper(root->right);
+                    break;
+                }
+                else
+                {
+                    root=root->right;
+                }
+            }
+        }
+        return dummy;
+    }
+};
