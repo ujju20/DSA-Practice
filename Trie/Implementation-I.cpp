74b82caeced54f095c6600920471a3bc85016059
Ujjawal Sharma <64632969+ujju20@users.noreply.github.com>
2022-02-16 16:07:47 +0530

    Create Implementation-I.cpp

---
+++ Trie/Implementation-I.cpp
@@ -0,0 +1,85 @@
+class Node{
+    Node* links[26];
+    bool flag=false;
+    public:
+    bool containsKey(char ch)
+    {
+        return links[ch-'a']!=NULL;
+    }
+    Node* get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void put(char ch,Node* node)
+    {
+        links[ch-'a']=node;
+    }
+    void end()
+    {
+        flag=true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+class Trie {
+    private: Node* root;
+public:
+    Trie() {
+        root= new Node();
+    }
+    
+    void insert(string word) {
+        Node* node=root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+        }
+        node->end();
+    }
+    
+    bool search(string word) {
+        Node* node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(node->containsKey(word[i]))
+            {
+                node=node->get(word[i]);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for(int i=0;i<prefix.size();i++)
+        {
+            if(node->containsKey(prefix[i]))
+            {
+                node=node->get(prefix[i]);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
