4e83c07f25a75838c988100f4a05205d5585b313
Ujjawal Sharma <64632969+ujju20@users.noreply.github.com>
2022-03-16 13:59:54 +0530

    Create Swap-Node-in-Pairs-Recursive.cpp

---
+++ LinkedList/Swap-Node-in-Pairs-Recursive.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head or !head->next)
+            return head;
+        
+        ListNode* remaining=head->next->next;
+        ListNode* newHead=head->next;
+        head->next->next=head;
+        head->next=swapPairs(remaining);
+        return newHead;
+    }
+};
